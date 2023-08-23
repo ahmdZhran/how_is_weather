568568d4a24aa2d303a987de526c38961b521c4e
Ahmed <ahmedmohameddx80@gmail.com>
2023-08-24 01:27:43 +0300

    set color to search screen

--- lib/presentation/search_page.dart
+++ lib/presentation/search_page.dart
@@ -5,6 +5,8 @@ class SearchPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Color(0xff34425b),
+    );
   }
 }
