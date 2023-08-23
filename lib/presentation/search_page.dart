a21c6d72d82cfbe0d36e6a938471698026226d6b
Ahmed <ahmedmohameddx80@gmail.com>
2023-08-24 01:47:17 +0300

    change icon and title

--- lib/presentation/search_page.dart
+++ lib/presentation/search_page.dart
@@ -8,9 +8,15 @@ class SearchPage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Color(0xff34425b),
       appBar: AppBar(
+        leading: Icon(
+          Icons.arrow_back_ios,
+          size: 28,
+        ),
+        elevation: 0,
         title: Text(
-          'weather app',
-          style: TextStyle(color: Colors.white),
+          'Weather',
+          style: TextStyle(
+              color: Colors.white, fontFamily: 'Tajawal', fontSize: 28),
         ),
         backgroundColor: Color(0xff34425b),
       ),
