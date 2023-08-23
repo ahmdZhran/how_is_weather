11a8f79ea7b0929fca59ae33f93e53a0458e1201
Ahmed <ahmedmohameddx80@gmail.com>
2023-08-23 08:26:18 +0300

    first initial

---
+++ android/app/src/main/kotlin/com/example/ui_for_weather/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ui_for_weather
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/presentation/home.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/starts_in_sky.jfif'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            SizedBox(height: 100),
+            Text(
+              'Your Content Here',
+              style: TextStyle(fontSize: 24, color: Colors.white),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
