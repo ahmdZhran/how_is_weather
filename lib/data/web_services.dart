2f2b000082b6092f4a26a4bbd32f7e9319d94186
Ahmed <ahmedmohameddx80@gmail.com>
2023-08-24 21:43:45 +0300

    import utl constants

--- lib/data/web_services.dart
+++ lib/data/web_services.dart
@@ -1,7 +1,13 @@
 import 'package:http/http.dart' as http;
 
+import '../utls/constants.dart';
+
 class ApiService {
   ApiService._instance();
   static final ApiService service = ApiService._instance();
-  fetchData(String cityName) {}
+  fetchData(String cityName) {
+    String url =
+        '$baseUrl/forecast.json?key=$apiKey&q=$cityName&days=1&aqi=no&alerts=no';
+    http.get(Uri.parse(url));
+  }
 }
