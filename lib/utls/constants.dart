e4b4ef7a28dbac435475fc5f2fd1f0fe78abd77c
Ahmed <ahmedmohameddx80@gmail.com>
2023-08-24 21:43:09 +0300

    get url and do a parse to it

--- lib/utls/constants.dart
+++ lib/utls/constants.dart
@@ -1,3 +1,5 @@
 import 'package:flutter/material.dart';
 
 const Color myCustomColor = Color(0xFF34425B);
+const String baseUrl = 'http://api.weatherapi.com/v1';
+const String apiKey = '4e16bd16122c4c559f3111224232503';
