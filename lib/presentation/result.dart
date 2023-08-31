e42559aee556a8baf7f25125d136d7ed5fe7b558
Ahmed <ahmedmohameddx80@gmail.com>
2023-08-31 20:23:10 +0300

    add watherResponse to can show data on ui

--- lib/presentation/result.dart
+++ lib/presentation/result.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:how_is_weather/data/remote.dart';
+import 'package:how_is_weather/models/weather_response.dart';
 
 import '../widgets/custome_text.dart';
 
 class ResultPage extends StatelessWidget {
-  const ResultPage({super.key});
-
+  String? cityName;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,44 +18,91 @@ class ResultPage extends StatelessWidget {
             height: double.infinity,
           ),
           Center(
-            child: Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(top: 100),
-                  child: CustomText(
-                    text: 'Alexandria',
-                    fontSize: 40,
-                    color: Colors.white,
-                  ),
-                ),
-                CustomText(
-                  text: '19C°',
-                  fontSize: 70,
-                  color: Colors.white,
-                ),
-                CustomText(
-                  text: 'Mostly Clear',
-                  fontSize: 20,
-                  color: Colors.white.withOpacity(0.8),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
+              // child: Column(
+              //   children: [
+              //     Padding(
+              //       padding: const EdgeInsets.only(top: 100),
+              //       child: CustomText(
+              //         text: 'Alexandria',
+              //         fontSize: 40,
+              //         color: Colors.white,
+              //       ),
+              //     ),
+              //     CustomText(
+              //       text: '19C°',
+              //       fontSize: 70,
+              //       color: Colors.white,
+              //     ),
+              //     CustomText(
+              //       text: 'Mostly Clear',
+              //       fontSize: 20,
+              //       color: Colors.white.withOpacity(0.8),
+              //     ),
+              //     Row(
+              //       mainAxisAlignment: MainAxisAlignment.center,
+              //       children: [
+              //         CustomText(
+              //           text: 'H:24°',
+              //           fontSize: 20,
+              //           color: Colors.white,
+              //         ),
+              //         SizedBox(width: 10),
+              //         CustomText(
+              //           text: 'L:18°',
+              //           fontSize: 20,
+              //           color: Colors.white,
+              //         )
+              //       ],
+              //     )
+              //   ],
+              // ),
+
+              ),
+          FutureBuilder(
+            future: ApiService.service.fetchData(cityName!),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                WeatherResponse weatherResponse = snapshot.data!;
+                return Column(
                   children: [
+                    Padding(
+                      padding: const EdgeInsets.only(top: 100),
+                      child: CustomText(
+                        text: 'Alexandria',
+                        fontSize: 40,
+                        color: Colors.white,
+                      ),
+                    ),
                     CustomText(
-                      text: 'H:24°',
-                      fontSize: 20,
+                      text: '19C°',
+                      fontSize: 70,
                       color: Colors.white,
                     ),
-                    SizedBox(width: 10),
                     CustomText(
-                      text: 'L:18°',
+                      text: 'Mostly Clear',
                       fontSize: 20,
-                      color: Colors.white,
+                      color: Colors.white.withOpacity(0.8),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        CustomText(
+                          text: 'H:24°',
+                          fontSize: 20,
+                          color: Colors.white,
+                        ),
+                        SizedBox(width: 10),
+                        CustomText(
+                          text: 'L:18°',
+                          fontSize: 20,
+                          color: Colors.white,
+                        )
+                      ],
                     )
                   ],
-                )
-              ],
-            ),
+                );
+              }
+            },
           )
         ],
       ),
