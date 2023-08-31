c010986c4e57650b79dcc4dffa7cac3158c880db
Ahmed <ahmedmohameddx80@gmail.com>
2023-08-31 15:55:42 +0300

    change content of home page

--- lib/presentation/home.dart
+++ lib/presentation/home.dart
@@ -43,41 +43,7 @@ class _HomeState extends State<Home> {
           ),
           Center(
             child: Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(top: 100),
-                  child: CustomText(
-                    text: 'Alexandria',
-                    fontSize: 40,
-                    color: Colors.white,
-                  ),
-                ),
-                CustomText(
-                  text: '19C°',
-                  fontSize: 70,
-                  color: Colors.white,
-                ),
-                CustomText(
-                  text: 'Mostly Clear',
-                  fontSize: 20,
-                  color: Colors.white.withOpacity(0.8),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    CustomText(
-                      text: 'H:24°',
-                      fontSize: 20,
-                      color: Colors.white,
-                    ),
-                    CustomText(
-                      text: 'L:18°',
-                      fontSize: 20,
-                      color: Colors.white,
-                    )
-                  ],
-                )
-              ],
+              children: [],
             ),
           )
         ],
