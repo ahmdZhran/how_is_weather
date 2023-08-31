d5e6c7c3ad81dd507ed2682a4d1183aae9a87f2a
Ahmed <ahmedmohameddx80@gmail.com>
2023-08-31 13:06:37 +0300

    make future bring data from WeatherResponse class

--- lib/data/remote.dart
+++ lib/data/remote.dart
@@ -1,3 +1,4 @@
+import 'package:how_is_weather/models/weather_response.dart';
 import 'package:http/http.dart' as http;
 
 import '../utls/constants.dart';
@@ -5,7 +6,7 @@ import '../utls/constants.dart';
 class ApiService {
   ApiService._instance();
   static final ApiService service = ApiService._instance();
-  Future<http.Response> fetchData(String cityName) async {
+  Future<WeatherResponse> fetchData(String cityName) async {
     String url =
         '$baseUrl/forecast.json?key=$apiKey&q=$cityName&days=1&aqi=no&alerts=no';
 
