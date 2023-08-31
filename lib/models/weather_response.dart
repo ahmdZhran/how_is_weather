5937c7ae603f67b5eee982b00820196f883048d1
Ahmed <ahmedmohameddx80@gmail.com>
2023-08-31 17:56:23 +0300

    adding condition class

--- lib/models/weather_response.dart
+++ lib/models/weather_response.dart
@@ -34,3 +34,13 @@ class Current {
         : null;
   }
 }
+
+class Condition {
+  String? text;
+
+  Condition({this.text});
+
+  Condition.fromJson(Map<String, dynamic> json) {
+    text = json['text'];
+  }
+}
