6dc4e2d304dcfe2049668d13810c524c18a93141
Ahmed <ahmedmohameddx80@gmail.com>
2023-08-31 12:50:42 +0300

    create cityName variable

--- lib/presentation/home.dart
+++ lib/presentation/home.dart
@@ -13,6 +13,7 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
+    String cityName;
     return Scaffold(
       floatingActionButtonLocation:
           FloatingActionButtonLocation.miniCenterFloat,
