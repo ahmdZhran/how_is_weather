5117f0ff29605e35c112afece6a1dbfe872c690c
Ahmed <ahmedmohameddx80@gmail.com>
2023-09-02 10:21:44 +0300

    make this selectedCity give its value to cityName

--- lib/presentation/home.dart
+++ lib/presentation/home.dart
@@ -41,7 +41,7 @@ class _HomeState extends State<Home> {
             );
             if (selectedCity != null) {
               setState(() {
-                cityName = selectedCity; // Update the cityName variable
+                cityName = selectedCity;
               });
             }
           },
