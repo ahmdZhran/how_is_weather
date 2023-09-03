aad24f52ccd17e2b207eeb2c673d3a6045fdf8a3
Ahmed <ahmedmohameddx80@gmail.com>
2023-09-03 15:38:41 +0300

    formatting code

--- lib/Cubit/weather_cubit.dart
+++ lib/Cubit/weather_cubit.dart
@@ -3,9 +3,7 @@ import 'package:how_is_weather/Cubit/weather_state.dart';
 import 'package:how_is_weather/models/weather_response.dart';
 
 class WeatherCubit extends Cubit<WeatherStates> {
-  WeatherCubit(
-    super.initialState,
-  );
+  WeatherCubit(super.initialState);
 
   void fetchWeather({required String cityName}) {}
 }
