c344df6dc7813af8821020536dd2be9ee909af25
Ahmed <ahmedmohameddx80@gmail.com>
2023-09-03 15:17:47 +0300

    make weatherCubit extends from weatherState

--- lib/Cubit/weather_cubit.dart
+++ lib/Cubit/weather_cubit.dart
@@ -1,1 +1,6 @@
-class WeatherCubit {}
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:how_is_weather/Cubit/weather_state.dart';
+
+class WeatherCubit extends Cubit<WeatherStates> {
+  WeatherCubit(super.initialState);
+}
