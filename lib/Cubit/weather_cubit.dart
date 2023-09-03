ff0bd1fe29ea470f04797f2db51f8db27fdef61f
Ahmed <ahmedmohameddx80@gmail.com>
2023-09-03 15:37:04 +0300

    created WeatherResponse to call apiService

--- lib/Cubit/weather_cubit.dart
+++ lib/Cubit/weather_cubit.dart
@@ -1,7 +1,9 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:how_is_weather/Cubit/weather_state.dart';
+import 'package:how_is_weather/models/weather_response.dart';
 
 class WeatherCubit extends Cubit<WeatherStates> {
-  WeatherCubit(super.initialState);
+  WeatherCubit(super.initialState, {this.weatherResponse});
+  WeatherResponse? weatherResponse;
   void fetchWeather({required String cityName}) {}
 }
