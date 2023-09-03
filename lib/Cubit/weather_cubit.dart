66cb454510b1c31bea97675febc93433ad4e8018
Ahmed <ahmedmohameddx80@gmail.com>
2023-09-03 15:44:15 +0300

    delete unnecessary import

--- lib/Cubit/weather_cubit.dart
+++ lib/Cubit/weather_cubit.dart
@@ -1,7 +1,6 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:how_is_weather/Cubit/weather_state.dart';
 import 'package:how_is_weather/data/remote.dart';
-import 'package:how_is_weather/models/weather_response.dart';
 
 class WeatherCubit extends Cubit<WeatherStates> {
   WeatherCubit(super.initialState, this.apiService);
